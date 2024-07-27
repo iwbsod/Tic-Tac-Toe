5a901d5c28dc3bd735505bb093b0b4d027674e35
Erin <erinrainabo35@gmail.com>
2024-07-27 18:05:40 +0400

    Create a match player function that matches the div cell position to the grid object position

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const humanScore = document.querySelector('js-human-score')
+const monkeyScore = document.querySelector('js-human-score')
 const robotScore = document.querySelector('js-robot-score')
 const gridDisplay = document.querySelector('.js-grid')
 const difficultyDisplay = document.querySelector('.js-difficulty-container')
@@ -10,11 +10,11 @@ const hardButton = document.querySelector('.js-hard-button')
 
 const generateGrid = () => {
   let gridHTML = ''
-    let cellIndex = 1
+    let cellIndex = 0
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         gridHTML += `
-          <div class="cell" id="${cellIndex}"></div>
+          <div class="cell js-cell" id="${cellIndex}"></div>
         `
         cellIndex++
       }
@@ -42,28 +42,78 @@ const chooseDifficulty = () => {
   })
 }
 
+//I figured out the problem, object.values' elements are arrays so if you compare them with object.every, it returns false since arrays are reference types
+const checkLines = (lineClassification, grid) => {
+  for (const [lineName, line] of Object.entries(grid[lineClassification])) {
+    const state = Object.values(line).map(subArray => subArray[0])
+    const player = Object.values(line).map(subArray => subArray[1])
+
+    console.log('object', Object.values(line))
+    console.log(state)
+    console.log(player)
+
+    //How to check if all is true and 
+
+    if (state.every(value => value === true) && player.every(value => value !== undefined)) {
+      console.log('congrats')
+    }
+  }
+}
+
+const matchPosition = (position, lineClassification, grid, player) => {
+  for (const [lineName, line] of Object.entries(grid[lineClassification])) {
+    for (const [cellName, cellValue] of Object.entries(line)) {
+      if (cellName === position) { 
+        if (cellValue.length !== 2) {
+          grid[lineClassification][lineName][cellName][0] = true
+          grid[lineClassification][lineName][cellName].push(player)
+        }
+      }
+    }
+  }
+}
+
+const playerMove = (cells, grid, player) => {
+  cells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+      cellId = cell.id 
+
+      matchPosition(cellId, 'rows', grid, player)
+      matchPosition(cellId, 'columns', grid, player)
+      matchPosition(cellId, 'diagonals', grid, player)
+      checkLines('rows', grid)
+      // checkLines('columns', grid)
+      // checkLines('diagonals', grid)
+
+      cell.innerHTML = `<img class="cell-image" src="assets/${player}.png" alt="">`
+    })
+  })
+}
+
 const startGame = () => {
   startGameButton.addEventListener('click', () => {
+    generateGrid()
+
     const grid = {
       rows: {
-        row1: {0: false, 1: false, 2: false}, //line
-        row2: {3: false, 4: false, 5: false},
-        row3: {6: false, 7: false, 8: false},
+        row1: {0: [false], 1: [false], 2: [false]},
+        row2: {3: [false], 4: [false], 5: [false]},
+        row3: {6: [false], 7: [false], 8: [false]},
       },
       columns: {
-        column1: {0: false, 3: false, 6: false},
-        column2: {1: false, 4: false, 7: false},
-        column3: {2: false, 5: false, 8: false},
+        column1: {0: [false], 3: [false], 6: [false]},
+        column2: {1: [false], 4: [false], 7: [false]},
+        column3: {2: [false], 5: [false], 8: [false]},
       },
       diagonals: {
-        diagonal1: {0: false, 4: false, 8: false},
-        diagonal2: {2: false, 4: false, 6: false},
+        diagonal1: {0: [false], 4: [false], 8: [false]},
+        diagonal2: {2: [false], 4: [false], 6: [false]},
       },
     }
     const difficulty = chooseDifficulty()
+    const cells = document.querySelectorAll('.js-cell')
     
-    generateGrid()
-
+    playerMove(cells, grid, 'monkey')
 
   })
 }
